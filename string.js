8d2475cadadb0af00c101ed11a29f022020ec770
Andre Bottentuit <andre.bottentuit@gmail.com>
2022-07-25 19:46:27 -0300

    inserção de script

---
+++ string.js
@@ -0,0 +1,6 @@
+let nome = 'andré'.toUpperCase() // MÉTODO PARA DEIXAR A STRING MAIÚSCULA
+let sobrenome = ' BOTTENTUIT'.toLowerCase() // MÉTODO PARA DEIXAR A STRING MINÚSCULA
+let quantidade = nome.length // OBJETO STRING PARA CONTAR QUANTOS CARACTERES TEM A STRING
+
+console.log(`${nome} tem ${quantidade} letras e seu nome completo é: ${nome.concat(sobrenome)}`) //CONCAT É UM MÉTODO PARA JUNTAR UMA STRING A OUTRA
+
